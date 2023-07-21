5a83ef67c6f0bd0930e1067b72a956ad37f63cab
mishgan55 <vorota.mogilev@gmail.com>
2023-07-21 18:32:17 +0300

    first Controller

--- src/main/java/khorsun/springtest/MVC/controllers/PeopleController.java
+++ src/main/java/khorsun/springtest/MVC/controllers/PeopleController.java
@@ -3,13 +3,19 @@ package khorsun.springtest.MVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/first")
 public class PeopleController {
     @GetMapping("/hello")
     public String sayHello(){
-        return "hello";
+        return "first/hello";
+    }
+    @GetMapping("goodbye")
+    public  String sayGoodBye(){
+        return "first/goodBye";
     }
 
 }
