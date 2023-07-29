320c0e0fa1ddefa250e989fbbb66b55dee9e4360
mishgan55 <vorota.mogilev@gmail.com>
2023-07-29 19:31:43 +0300

    1.Add Hibernate to project 2.Remade model person for Hibernate 3.Start writing CRUD methods using Hibernate

--- src/main/java/khorsun/springtest/MVC/dao/PersonDAO.java
+++ src/main/java/khorsun/springtest/MVC/dao/PersonDAO.java
@@ -2,52 +2,62 @@ package khorsun.springtest.MVC.dao;
 
 
 import khorsun.springtest.MVC.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Component
 public class PersonDAO {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SessionFactory sessionFactory;
+
 @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
+    public PersonDAO(JdbcTemplate jdbcTemplate,  SessionFactory sessionFactory1) {
         this.jdbcTemplate = jdbcTemplate;
-    }
+        this.sessionFactory = sessionFactory1;
+}
 
     public Person show(int id) {
 
-        return  jdbcTemplate.query("SELECT * FROM spring_app.public.person WHERE person_id=?",new Object[]{id},
+        return  jdbcTemplate.query("SELECT * FROM hibernate_demo_db.public.person WHERE person_id=?",new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
     public Person show(String email){
-        return  jdbcTemplate.query("select * from spring_app.public.person where email=?",new Object[]{email},
+        return  jdbcTemplate.query("select * from hibernate_demo_db.public.person where email=?",new Object[]{email},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
-
+    @Transactional(readOnly = true)
     public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Person> resultList = session.createQuery("select p from Person p", Person.class).getResultList();
+        System.out.println(resultList);
 
-        return jdbcTemplate.query("SELECT * FROM spring_app.public.person",
-                new BeanPropertyRowMapper<>(Person.class));
+
+        return resultList;
     }
 
     public void create(Person person) {
 
-    jdbcTemplate.update("insert into spring_app.public.person(name, age, email) values (?,?,?)",
+    jdbcTemplate.update("insert into hibernate_demo_db.public.person(name, age, email) values (?,?,?)",
             person.getName(),person.getAge(),person.getEmail());
 
     }
 
     public void edit(Person person,int id) {
-    jdbcTemplate.update("update spring_app.public.person set name=?,age=?,email=? where person_id=?",
+    jdbcTemplate.update("update hibernate_demo_db.public.person set name=?,age=?,email=? where person_id=?",
             person.getName(),person.getAge(),person.getEmail(),id);
     }
 
     public void delete(int id)  {
 
-    jdbcTemplate.update("delete from spring_app.public.person where person_id=?",id);
+    jdbcTemplate.update("delete from hibernate_demo_db.public.person where person_id=?",id);
 
 
     }
--- src/main/java/khorsun/springtest/MVC/models/Person.java
+++ src/main/java/khorsun/springtest/MVC/models/Person.java
@@ -1,27 +1,39 @@
 package khorsun.springtest.MVC.models;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
     private int personId;
     @NotEmpty(message = "name should not be empty")
     @Size(max = 30, message = "name should be between 0 and 30 characters")
+    @Column(name = "name")
     private String name;
     @Min( value = 0, message = "Age should be grater then 0")
+    @Column(name = "age")
     private int age;
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Please write down valid email")
+    @Column(name = "email")
     private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name, int age, String email) {
-        this.personId = id;
+    public Person( String name, int age, String email) {
         this.name = name;
         this.age = age;
         this.email = email;
